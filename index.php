2d6e775a484a1da878ae37b37a6fa689995bf2e1
Alexandre Le Borgne <a.leborgne@healabs.fr>
2024-04-11 14:12:09 +0200

    remove useless comments, add missing array denormalizer

--- index.php
+++ index.php
@@ -5,33 +5,26 @@ use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\PropertyInfo\Extractor\PhpStanExtractor;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-// a full list of extractors is shown further below
 $phpDocExtractor = new PhpDocExtractor();
 $phpStanExtractor = new PhpStanExtractor();
 $reflectionExtractor = new ReflectionExtractor();
 
-// list of PropertyListExtractorInterface (any iterable)
 $listExtractors = [$reflectionExtractor];
 
-// list of PropertyTypeExtractorInterface (any iterable)
 $typeExtractors = [
-    $phpStanExtractor,
-//    $phpDocExtractor,
+    $phpStanExtractor, // Comment this line to avoid the bug
+    $phpDocExtractor,
     $reflectionExtractor,
 ];
 
-// list of PropertyDescriptionExtractorInterface (any iterable)
 $descriptionExtractors = [$phpDocExtractor];
-
-// list of PropertyAccessExtractorInterface (any iterable)
 $accessExtractors = [$reflectionExtractor];
-
-// list of PropertyInitializableExtractorInterface (any iterable)
 $propertyInitializableExtractors = [$reflectionExtractor];
 
 $propertyInfo = new PropertyInfoExtractor(
@@ -42,20 +35,22 @@ $propertyInfo = new PropertyInfoExtractor(
     $propertyInitializableExtractors
 );
 
-// see below for more examples
 $properties = $propertyInfo->getTypes(Example::class, 'children');
 
 // MUST returns ""App\Child\ExampleChild"" but it returns "App\Example\ExampleChild"
 dump($properties[0]->getCollectionValueTypes()[0]->getClassName());
 
-$normalizers = [new ObjectNormalizer(propertyTypeExtractor: $propertyInfo)];
+$normalizers = [
+    new ArrayDenormalizer(),
+    new ObjectNormalizer(propertyTypeExtractor: $propertyInfo),
+];
 
 $serializer = new Serializer($normalizers);
 
 $result = $serializer->denormalize([
-    'children' => [
+    'children' => [[
         'testProperty' => '123',
-    ],
+    ]],
 ], Example::class);
 
 dump($result);
